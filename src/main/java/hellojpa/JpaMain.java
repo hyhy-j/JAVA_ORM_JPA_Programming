c4ec4a00e8994ba59088119c4a093cee02b6efa2
hyhy-j <lhjlhj10088@gmail.com>
2025-03-29 22:08:31 +0900

    [Feat] Member 엔티티 생성 및 persist 로직 구현

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -8,9 +8,23 @@ public class JpaMain {
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
-        //code
 
-        em.close();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member member = new Member();
+            member.setId(2L);
+            member.setName("HelloB");
+
+            em.persist(member);
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
         emf.close();
     }
 }
