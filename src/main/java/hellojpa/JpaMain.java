e5692de2a7cfa42230529ac442abc3e2568c394b
hyhy-j <lhjlhj10088@gmail.com>
2025-03-29 22:28:55 +0900

    [Feat] 회원 목록을 리스트로 가져오는 쿼리문 작성

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -2,6 +2,8 @@ package hellojpa;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 public class JpaMain {
 
     public static void main(String[] args) {
@@ -14,9 +16,15 @@ public class JpaMain {
 
         try {
 
-            Member findMember = em.find(Member.class, 1L);
-            findMember.setName("HelloJPA");
+            /*Member findMember = em.find(Member.class, 1L);*/
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(5)
+                    .setMaxResults(8)
+                    .getResultList();
 
+            for (Member member : result) {
+                System.out.println("member.name = " + member.getName());
+            }
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
